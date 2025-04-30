0e3373e9f6423e823c7f31df60e6295ff339e052
RYUJEONGHUN <ryugh0210@naver.com>
2025-04-30 17:55:49 +0900

    에러처리 handler

---
+++ src/main/java/UNITON/demo/login/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package UNITON.demo.login.exception;
+
+import UNITON.demo.login.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ResponseDto<Void>> handleIllegalArgument(IllegalArgumentException ex) {
+        ResponseDto<Void> response = ResponseDto.<Void>builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
