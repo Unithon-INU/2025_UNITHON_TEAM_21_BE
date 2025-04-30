e725cbbf60e5e88dba0d5881658057872a3e2a2a
RYUJEONGHUN <ryugh0210@naver.com>
2025-04-30 17:06:13 +0900

    일반 회원가입 & oauth 회원가입 구현

---
+++ src/main/java/UNITON/demo/login/service/JoinService.java
@@ -0,0 +1,49 @@
+package UNITON.demo.login.service;
+
+import UNITON.demo.login.dto.JoinByEmailDto;
+import UNITON.demo.login.dto.JoinByOAuthDto;
+import UNITON.demo.login.entity.UserEntity;
+import UNITON.demo.login.entity.UserRole;
+import UNITON.demo.login.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userRepository=userRepository;
+        this.bCryptPasswordEncoder=bCryptPasswordEncoder;
+    }
+
+    public void joinByEmail(JoinByEmailDto dto) {
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+
+        UserEntity user =createBasicUser(dto.getEmail(), dto.getNickname());
+        user.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void joinByOAuth(JoinByOAuthDto dto) {
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+
+        UserEntity user =createBasicUser(dto.getEmail(), dto.getNickname());
+        user.setProvider(dto.getProvider());
+        user.setProviderId(dto.getProviderId());
+        userRepository.save(user);
+    }
+
+    private UserEntity createBasicUser(String email, String nickname) {
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
+        user.setNickname(nickname);
+        user.setRole(UserRole.USER);
+        return user;
+    }
+}
